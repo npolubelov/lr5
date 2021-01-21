3b11aad142604454c5e9272dede916cb1395998c
npolubelov <hampval@gmail.com>
2021-01-21 13:36:38 +0700

    Расположение кнопок в ряд

--- lab5.py
+++ lab5.py
@@ -2,7 +2,7 @@
 
 import sys
 from PyQt5.QtWidgets import (QWidget, QToolTip,
-    QPushButton, QApplication, QMainWindow, QLabel)
+    QPushButton, QApplication, QMainWindow, QLabel, QHBoxLayout, QVBoxLayout)
 from PyQt5.QtGui import QFont
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QCoreApplication
@@ -16,20 +16,27 @@ class Example(QMainWindow, QWidget):
 
 
     def initUI(self):
+
         self.statusBar().showMessage('Состояние')
+        
+        btn1 = QPushButton('выход', self)
+        btn1.clicked.connect(QCoreApplication.instance().quit)
+        btn1.move(50, 10)
 
-        btn = QPushButton('выход', self)
-        btn.clicked.connect(QCoreApplication.instance().quit)
-        btn.resize(btn.sizeHint())
-        btn.move(10, 10)
+        btn2 = QPushButton('выход2', self)
+        btn2.clicked.connect(QCoreApplication.instance().quit)
+        btn2.move(200, 10)
 
+        btn3 = QPushButton('выход3', self)
+        btn3.clicked.connect(QCoreApplication.instance().quit)
+        btn3.move(350, 10)
+            
         lbl1 = QLabel('label1', self)
         lbl1.move(10, 40)
 
-        self.setGeometry(0, 0, 300, 300)
-        self.setWindowTitle('test5')
+        self.setGeometry(0, 0, 600, 600)
+        self.setWindowTitle('test6')
         self.setWindowIcon(QIcon('bit.png'))
-
         self.show()
 
 
