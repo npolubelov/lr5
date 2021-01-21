d3839e008507d9cab4db1259a12ecdcf09385565
npolubelov <hampval@gmail.com>
2021-01-21 13:16:05 +0700

    Функциональная кнопк + строка состояния

--- lab5.py
+++ lab5.py
@@ -2,11 +2,12 @@
 
 import sys
 from PyQt5.QtWidgets import (QWidget, QToolTip,
-    QPushButton, QApplication)
+    QPushButton, QApplication, QMainWindow)
 from PyQt5.QtGui import QFont
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QCoreApplication
 
-class Example(QWidget):
+class Example(QMainWindow):
 
     def __init__(self):
         super().__init__()
@@ -15,13 +16,15 @@ class Example(QWidget):
 
 
     def initUI(self):
+        self.statusBar().showMessage('Состояние')
 
-        btn = QPushButton('кнопка1', self)
+        btn = QPushButton('выход', self)
+        btn.clicked.connect(QCoreApplication.instance().quit)
         btn.resize(btn.sizeHint())
         btn.move(10, 10)
 
         self.setGeometry(0, 0, 300, 300)
-        self.setWindowTitle('test3')
+        self.setWindowTitle('test5')
         self.setWindowIcon(QIcon('bit.png'))
 
         self.show()
